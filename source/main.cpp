365fd70152576b87463a69ec86b5bc2758708e29
NgHuuTrong <trongnh2003@gmail.com>
2022-10-29 09:21:59 +0700

    add raygui

--- source/main.cpp
+++ source/main.cpp
@@ -1,15 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <raylib.h>
-
+#define RAYGUI_IMPLEMENTATION
+#include "../header/raygui.h"
 
 int main()
 {
-	InitWindow(800, 600, "Game");
+	InitWindow(1000, 600, "Game");
 	SetTargetFPS(60);
+	GuiSetStyle(DEFAULT, TEXT_SIZE, 25);
 	while (!WindowShouldClose())
 	{
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
+		if (GuiButton({ 100,100,100,60 }, "Button"))
+		{
+			DrawText("Test Button", 350, 150, 30, RED);
+		}
 		DrawText("Game Bang Qua Duong", 350, 250, 30, BLACK);
 		EndDrawing();
 	}

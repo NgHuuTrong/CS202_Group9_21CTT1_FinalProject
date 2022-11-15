15a6edde274132cd51e305386978f4379cf6b740
NgHuuTrong <trongnh2003@gmail.com>
2022-11-15 11:08:09 +0700

    Update continue button

--- source/home.cpp
+++ source/home.cpp
@@ -14,8 +14,8 @@ Screen Home::update()
 		if (mode == 1)
 		{
 			allLane.clear();
-			allLane = random(15);
 		}
+		else if (mode == 2) mode = 1;
 		int tmp = mode;
 		mode = 0;
 		return static_cast<Screen> (tmp);
@@ -26,9 +26,11 @@ Screen Home::update()
 void Home::draw()
 {
 	DrawTexture(homeBackground, 0, 0, WHITE);
-	if (GuiButton({ 500, 150, 280, 70 }, "PLAY GAME"))
+	if (allLane.size())
+		if (GuiButton({ 500, 150, 280, 70 }, "CONTINUE"))
+			mode = 2;
+	if (GuiButton({ 500, 230, 280, 70 }, "PLAY GAME"))
 		mode = 1;
-	GuiButton({ 500, 230, 280, 70 }, "CONTINUE");
 	GuiButton({ 500, 310, 280, 70 }, "HIGHSCORE");
 	GuiButton({ 500, 390, 280, 70 }, "INSTRUCTION");
 	GuiButton({ 500, 470, 280, 70 }, "EXIT");
